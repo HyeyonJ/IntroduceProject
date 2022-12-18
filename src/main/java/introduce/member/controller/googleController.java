8ace0142257837fabe5c89848367c33d08bd17b2
HyeyeonJ <michaelvia29@gmail.com>
2022-12-18 22:07:58 +0900

    git branch test2

---
+++ src/main/java/introduce/member/controller/googleController.java
@@ -0,0 +1,14 @@
+package introduce.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class googleController {
+
+	// 기본 페이지 요청 메서드
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
