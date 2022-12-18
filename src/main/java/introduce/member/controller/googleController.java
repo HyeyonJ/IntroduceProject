2323ecec7b1ed4a0d229d2e29a56ef04a0ae21bf
HyeyeonJ <michaelvia29@gmail.com>
2022-12-18 22:18:53 +0900

    브랜치 연습

--- src/main/java/introduce/member/controller/googleController.java
+++ src/main/java/introduce/member/controller/googleController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class googleController {
-
+	// 기본 메서드 말고 소셜로 생성하기
 	// 기본 페이지 요청 메서드
 	@GetMapping("/")
 	public String index() {
